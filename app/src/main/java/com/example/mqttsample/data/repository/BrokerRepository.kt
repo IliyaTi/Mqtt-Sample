e8eddc3f57575be2f9141259e2d6853aea358d8c
Ali Falahati <a.falahati@sahar.net>
2025-05-12 11:49:27 +0330

    init repository

---
+++ app/src/main/java/com/example/mqttsample/data/repository/BrokerRepository.kt
@@ -0,0 +1,16 @@
+package com.example.mqttsample.data.repository
+
+import com.example.mqttsample.data.source.local.dao.BrokerDao
+import com.example.mqttsample.data.source.local.dao.DeviceDao
+
+class BrokerRepository (
+    private val brokerDao: BrokerDao,
+    private val deviceDao: DeviceDao
+) {
+
+
+
+
+
+
+}
